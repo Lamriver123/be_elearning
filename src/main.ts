0149429a77aeb3e7d494f84721a839fbb9981ae9
Lamriver123 <ngoclamhttv@gmail.com>
2026-08-03 15:27:59 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: configService.get<string>('CORS_ORIGIN', 'http://localhost:5173'),
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
